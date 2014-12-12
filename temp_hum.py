008ab15bc54501124e6cfd328d3c98b1eaa4c3e8
Nick <nthrockmorton@users.noreply.github.com>
2014-12-11 21:35:39 -0600

    Update temp_hum.py

--- temp_hum.py
+++ temp_hum.py
@@ -41,8 +41,9 @@ for i in range(0,10):
         if humidity is not None and temperature is not None:
                         timestamp = time.strftime('%b %d, %Y  %I:%M:%S %P')
                         temp_hum = 'Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_f, humidity)
-                        json.dump(timestamp, file\n)
-                        json.dump(temp_hum, file\n)
+                        json.dump(timestamp, file)
+                        json.dump(\n)
+                        json.dump(temp_hum, file)\n
                         i += 1
                         time.sleep(5)
         else:
