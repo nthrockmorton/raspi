cbf8044eec04f152a1c1d8faca6951717ac1a110
Nick <nthrockmorton@users.noreply.github.com>
2014-12-11 21:15:13 -0600

    Update temp_hum.py

--- temp_hum.py
+++ temp_hum.py
@@ -38,9 +38,11 @@ for i in range(0,10):
         temperature_f = temperature * 1.8 +32
         i = 0
         if humidity is not None and temperature is not None:
+                        timestamp = time.strftime('%b %d, %Y  %I:%M:%S %P')
+                        temp_hum = 'Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_f, humidity)
                         print time.strftime('%b %d, %Y  %I:%M:%S %P')
                         print 'Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_f, humidity)
-                        file.write((time.strftime('%b %d, %Y %I:%M:%S %P')),'{0:0.1f}*F {1:0.1f}%'.format(temperature_f, humidity))
+                        file.write(timestamp, temp_hum)
                         i += 1
                         time.sleep(5)
         else:
