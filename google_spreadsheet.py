c8d6d127acd39eab48f831921d1d5875292dbc80
Nick <nthrockmorton@users.noreply.github.com>
2014-12-17 14:30:47 -0600

    Update google_spreadsheet.py

--- google_spreadsheet.py
+++ google_spreadsheet.py
@@ -1,31 +1,4 @@
-#!/usr/bin/python
 
-# Google Spreadsheet DHT Sensor Data-logging Example
-
-# Depends on the 'gspread' package being installed.  If you have pip installed
-# execute:
-#   sudo pip install gspread
-
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import sys
 import time
 import datetime
@@ -97,3 +70,24 @@ while True:
 	# Wait 30 seconds before continuing
 	print ('Wrote a row to {0}').format(GDOCS_SPREADSHEET_NAME)
 	time.sleep(FREQUENCY_SECONDS)
+
+#!/usr/bin/python
+
+# Google Spreadsheet DHT Sensor Data-logging Example
+
+# Depends on the 'gspread' package being installed.  If you have pip installed
+# execute:
+#   sudo pip install gspread
+
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
